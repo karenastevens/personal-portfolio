c044bac16fb18b864bc79c99021a79b5465866ba
Karena Stevens <kaymark09@gmail.com>
2022-09-30 20:28:05 -0400

    removed unwanted files

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
-});
